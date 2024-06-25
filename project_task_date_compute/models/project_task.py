1d04d78f221618d38548dcdf1d982a45b582c233
Unai Beristain <unaiberis@gmail.com>
2024-06-25 16:26:25 +0200

    [ADD] Add module project_task_date_compute

---
+++ project_task_date_compute/models/project_task.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class ProjectTask(models.Model):
+    _inherit = "project.task"
+
+    start_date = fields.Date(
+        string="Start Date", 
+        compute="_compute_start_date", 
+        store=True
+    )
+    end_date = fields.Date(
+        string="End Date", 
+        compute="_compute_end_date", 
+        store=True
+    )
+
+    @api.depends("timesheet_ids.date")
+    def _compute_start_date(self):
+        for task in self:
+            task.start_date = min(task.timesheet_ids.mapped('date'), default=False)
+
+    @api.depends("timesheet_ids.date")
+    def _compute_end_date(self):
+        for task in self:
+            if task.stage_id.fold:
+                task.end_date = max(task.timesheet_ids.mapped('date'), default=False)
+            else:
+                task.end_date = False
+
